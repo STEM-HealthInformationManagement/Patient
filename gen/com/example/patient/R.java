d5691aa93ab6fc1a26fdebda4770027942813d9a
cli4ever <c.saintl.c@gmail.com>
2014-06-24 15:09:56 -0400

    fixed the scroller on start page

--- gen/com/example/patient/R.java
+++ gen/com/example/patient/R.java
@@ -36,17 +36,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int changedcolor=0x7f020000;
-        public static final int delete=0x7f02000b;
-        public static final int editbg=0x7f020001;
-        public static final int editcolorchange=0x7f020002;
-        public static final int ic_action_name=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo=0x7f020005;
-        public static final int thanks_bg=0x7f020006;
-        public static final int thx_bg=0x7f020007;
-        public static final int ty_bg=0x7f020008;
-        public static final int ty_layout_bg=0x7f020009;
-        public static final int up_button=0x7f02000a;
+        public static final int delete=0x7f02000c;
+        public static final int down_button=0x7f020001;
+        public static final int editbg=0x7f020002;
+        public static final int editcolorchange=0x7f020003;
+        public static final int ic_action_name=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int logo=0x7f020006;
+        public static final int thanks_bg=0x7f020007;
+        public static final int thx_bg=0x7f020008;
+        public static final int ty_bg=0x7f020009;
+        public static final int ty_layout_bg=0x7f02000a;
+        public static final int up_button=0x7f02000b;
     }
     public static final class id {
         public static final int Button01=0x7f0b004e;
@@ -58,13 +59,13 @@ public final class R {
         public static final int ChB_secondary=0x7f0b009b;
         public static final int ChB_self_insured=0x7f0b0098;
         public static final int HIS_Info=0x7f0b0003;
-        public static final int ImageView_logo=0x7f0b0006;
+        public static final int ImageView_logo=0x7f0b0004;
         public static final int RelativeLayout1=0x7f0b0056;
         public static final int ScrollView1=0x7f0b0055;
         public static final int ScrollView2=0x7f0b0078;
         public static final int action_settings=0x7f0b00d8;
         public static final int allergy_title=0x7f0b00be;
-        public static final int btn_Confirm=0x7f0b0004;
+        public static final int btn_Confirm=0x7f0b0006;
         public static final int button1=0x7f0b000a;
         public static final int button2=0x7f0b00bd;
         public static final int button3=0x7f0b00d7;
@@ -134,7 +135,7 @@ public final class R {
         public static final int httpResponseTextView=0x7f0b0091;
         public static final int imageButton_date=0x7f0b0070;
         public static final int item1=0x7f0b00d9;
-        public static final int progressBar1=0x7f0b0007;
+        public static final int progressBar1=0x7f0b0005;
         public static final int radioButton_Married=0x7f0b005e;
         public static final int radioButton_Single=0x7f0b005d;
         public static final int radioButton_female=0x7f0b0069;
@@ -265,7 +266,7 @@ public final class R {
         public static final int textView_state=0x7f0b0071;
         public static final int textView_title1=0x7f0b0057;
         public static final int textView_zipcode=0x7f0b006f;
-        public static final int update_Btn=0x7f0b0005;
+        public static final int update_Btn=0x7f0b0007;
         public static final int viewText_allergy1=0x7f0b00c5;
     }
     public static final class integer {
